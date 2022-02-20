3f3edc755fb52b640a58206fc9e6d7b6b2492723
jjtroiano0272@eagle.fgcu.edu <jjtroiano0272@eagle.fgcu.edu>
2022-02-20 15:07:05 -0500

    Fixed???

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import $ from 'jquery';
-import axios from 'axios';
-import cors from 'cors';
-// import dotenv from 'dotenv'; THIS WAS CAUSING BUGS
-// import express from 'express';
 import './index.css';
 import './components/common/rainbow-spin-loader.css';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -34,26 +30,6 @@ $(document).click(event => {
 // TODO: Learn how to send store this in an .env var or similar
 const domain = 'dev-b70h-mzq.us.auth0.com';
 const clientId = 'aPdJ4ZzQP40iUvtMR2CSC3siJfAPd8tz';
-
-const apiOptions = {
-  method: 'GET',
-  url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
-  params: { count: '5', wordLength: '5' },
-  headers: {
-    'x-rapidapi-host': 'random-words5.p.rapidapi.com',
-    'x-rapidapi-key': 'ff25be3b77msh3b29f7f8eea09bap187d99jsn385e047fde4c',
-  },
-};
-
-axios
-  .request(apiOptions)
-  .then(function (response) {
-    const foo = JSON.stringify(response.data[0]).toUpperCase();
-    console.log(foo);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
 
 ReactDOM.render(
   <Router>
