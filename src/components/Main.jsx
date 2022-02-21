8041afa32fdcb73ebbd293f86f2e90bdaea323be
jjtroiano0272@eagle.fgcu.edu <jjtroiano0272@eagle.fgcu.edu>
2022-02-21 14:24:41 -0500

    Fixes keyboard!

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -105,7 +105,7 @@ export default function Main(props) {
     return (
       <button
         className='btn'
-        id={index}
+        id={key}
         onClick={() => handleClick(key)}
         key={index}
       >
@@ -224,20 +224,47 @@ export default function Main(props) {
     const rowTiles = document.querySelector(
       '#guessRow-' + currentRow
     ).childNodes;
+    let checkWordle = wordle;
+    const guess = [];
+
+    rowTiles.forEach(tile => {
+      guess.push({ letter: tile.getAttribute('data'), color: 'grey-overlay' });
+    });
+
+    // rowTiles.forEach((tile, index) => {
+    //   const dataLetter = tile.getAttribute('data');
+
+    //   setTimeout(() => {
+    //     tile.classList.add('flip');
+    //     if (dataLetter == wordle[index]) {
+    //       tile.classList.add('green-overlay');
+    //       addColorToKey(dataLetter, 'green-overlay');
+    //     } else if (wordle.includes(dataLetter)) {
+    //       tile.classList.add('yellow-overlay');
+    //     } else {
+    //       tile.classList.add('grey-overlay');
+    //     }
+    //   }, 500 * index);
+    // });
+    guess.forEach((guess, index) => {
+      if (guess.letter == wordle[index]) {
+        guess.color = 'green-overlay';
+        checkWordle = checkWordle.replace(guess.letter, '');
+      }
+    });
+
+    guess.forEach(guess => {
+      if (checkWordle.includes(guess.letter)) {
+        guess.color = 'yellow-overlay';
+        checkWordle = checkWordle.replace(guess.letter, '');
+      }
+    });
 
     rowTiles.forEach((tile, index) => {
-      const dataLetter = tile.getAttribute('data');
-
       setTimeout(() => {
         tile.classList.add('flip');
-        if (dataLetter == wordle[index]) {
-          tile.classList.add('green-overlay');
-          addColorToKey(dataLetter, 'green-overlay');
-        } else if (wordle.includes(dataLetter)) {
-          tile.classList.add('yellow-overlay');
-        } else {
-          tile.classList.add('grey-overlay');
-        }
+        tile.classList.add(guess[index].color);
+        addColorToKey(guess[index].letter, guess[index].color);
       }, 500 * index);
     });
   };
